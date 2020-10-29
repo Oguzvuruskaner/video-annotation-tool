871444efd1de65d6b4649ad64bea02353503c72c
Oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-10-30 01:46:48 +0300

    File reducer has been added.

--- src/reducers/file.js
+++ src/reducers/file.js
@@ -1,6 +1,13 @@
-import {CLOSE_FILE,UPLOAD_FILE,SAVE_FILE} from "../actions"
+import {CLOSE_FILE,UPLOAD_FILE} from "../actions"
 
 export default (state = null,{type,payload}) => {
 
-    return state
+    switch (type) {
+        case UPLOAD_FILE:
+            return payload
+        case CLOSE_FILE:
+            return null
+        default:
+            return state
+    }
 }
