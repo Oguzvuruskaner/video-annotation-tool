d885e2825db91897041c372115408297d5158290
Oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-11-06 17:12:18 +0300

    Added video preview thumbnail.

--- src/components/VideoStrategy/VideoController/VideoSlider.js
+++ src/components/VideoStrategy/VideoController/VideoSlider.js
@@ -1,13 +1,16 @@
 import React,{Component} from "react"
 import {connect} from "react-redux"
-import {SET_TIME, UPDATE_TIME} from "../../../actions";
+import {SET_TIME} from "../../../actions";
 
 class VideoSlider extends Component{
 
     constructor(props) {
         super(props)
         this.sliderRef = React.createRef()
+        this.innerVideoRef = React.createRef()
+
         this.state = {
+            viewX:0,
             mouseoverFill:0
         }
     }
@@ -17,14 +20,18 @@ class VideoSlider extends Component{
         const {current} = this.sliderRef
 
         current.addEventListener("mousemove",(event)=>{
+            const {duration} = this.innerVideoRef.current
+
+            this.innerVideoRef.current.currentTime = event.clientX/current.clientWidth*duration
 
             this.setState({
-                mouseoverFill:event.clientX/current.clientWidth
+                mouseoverFill:event.clientX/current.clientWidth,
+                viewX:Math.min(current.clientWidth-320,event.clientX)
             })
         })
 
         current.addEventListener("mouseleave",() => {
-            this.setState({mouseoverFill:0})
+            this.setState({mouseoverFill:0,viewX:0})
         })
 
 
@@ -32,8 +39,9 @@ class VideoSlider extends Component{
     }
 
     onThumbnailClick = (event) => {
-        const {duration,setTime} = this.props
+        const {setTime} = this.props
         const {current} = this.sliderRef
+        const {duration} = this.innerVideoRef.current
 
         setTime(Math.min(duration,event.clientX/current.clientWidth*duration))
     }
@@ -44,8 +52,9 @@ class VideoSlider extends Component{
     }
 
     render() {
-        const {src,currentTime,duration} = this.props
-        const {mouseoverFill} = this.state
+        const {duration} = this.innerVideoRef.current || 0
+        const {src,currentTime} = this.props
+        const {mouseoverFill,viewX} = this.state
 
         const progressStyle = {
             width:`${currentTime/duration*100}%`
@@ -55,18 +64,24 @@ class VideoSlider extends Component{
             width:`${mouseoverFill * 100}%`
         }
 
+        const innerVideoStyle = {
+            display:(mouseoverFill === 0) ? "none" : "block",
+            left:viewX
+        }
+
         return <div ref={this.sliderRef} onClick={this.onThumbnailClick} className={"video-controller__video-slider"}>
             <div style={ progressStyle } className={"video-controller__video-slider__watch-progress"}/>
             <div  style={ thumbnailStyle }  className={"video-controller__video-slider__thumbnail"}/>
-            <video autoPlay={false} src={src} className={"video-controller__video-slider__inner-video"}/>
+            <div style={innerVideoStyle} className={"video-controller__video-slider__inner-video"}>
+                <video ref={this.innerVideoRef}  autoPlay={false} src={src} className={"video-controller__video-slider__inner-video__media"}/>
+            </div>
         </div>
     }
 }
 
 const mapStateToProps = ({file:{content},videoControl:{currentTime,duration}}) => ({
     src:content,
-    currentTime,
-    duration
+    currentTime
 })
 
 const mapDispatchToProps = (dispatch) => ({
