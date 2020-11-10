b13a8cd0ddfb7de51c300e3411045a4ab45cff02
Oguzvuruskaner <oguzvuruskaner97@hotmail.com>
2020-11-10 12:59:08 +0300

    Dynamically added ignored actions list to logger.

--- src/store.js
+++ src/store.js
@@ -4,10 +4,25 @@ import reduxThunk from "redux-thunk"
 import {createLogger} from "redux-logger"
 import reducers from "./reducers"
 import sagas from "./sagas"
+import {MOVE_INTERPOLATION, SET_CURRENT_TIME, SORT_INTERVAL, UPDATE_STATE_TIME} from "./actions/video";
+
+const IGNORED_ACTIONS = [
+    MOVE_INTERPOLATION,
+    SORT_INTERVAL,
+    UPDATE_STATE_TIME,
+    SET_CURRENT_TIME
+]
+
+const predicate = (ignoredActions) => (getState,{type,_}) => {
+    for(let actionType of ignoredActions){
+        if(actionType === type) return false
+    }
+    return true
+}
 
 const sagaMiddleware = createSagaMiddleware()
 const logger = createLogger({
-    predicate:(getState, action) => !action.type.includes('move') && !action.type.includes('sort')
+    predicate:predicate(IGNORED_ACTIONS)
 })
 
 const store = createStore(
